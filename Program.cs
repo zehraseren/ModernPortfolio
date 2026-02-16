2c5cfb494e1370ddbc12182aec6b0fab3bf9dbc9
Zehra Seren <fatmazehraseren@gmail.com>
2026-02-16 13:54:02 +0300

    feat(docker): add postgres infrastructure

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
